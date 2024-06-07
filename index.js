9b3a4b0ca0a7662bae4967f2d406221137141d91
nodus8tollens <mladenovskipredrag@gmail.com>
2024-06-07 11:14:55 +0200

    phonebook backend step 4

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3001;
 
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -41,6 +41,12 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
 });
 
 app.get("/info", (req, res) => {
